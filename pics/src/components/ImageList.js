99eb36c4e43c1c86b46d137645011a360d936612
zuodong <ehhhhhha@gmail.com>
2019-10-01 03:52:59 +1000

    ADD: ImageList

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    return <div>{props.images}</div>;
+    const images = props.images.map((image) => {
+        return <img key={image.id} src={image.urls.regular} alt={image.description} />
+    });
+    return <div>{images}</div>
 }
 
 export default ImageList;

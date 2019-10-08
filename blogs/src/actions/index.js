da6509235e7b575b1e886edc2ffc6372bee818ae
zuodong <ehhhhhha@gmail.com>
2019-10-09 01:37:32 +1100

    ADD: blog project

---
+++ blogs/src/actions/index.js
@@ -0,0 +1,14 @@
+import jsonPlaceholder from '../APIs/jsonPlaceHolder';
+
+
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response
+    });
+};
+
+
+
